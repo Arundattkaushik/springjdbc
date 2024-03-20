c0662c877d5c881ed415b418cb09c27cc3d9e6ac
Arun <Arundattkaushik>
2024-03-20 10:57:11 +0530

    jdbc configration set in springjdbc.xml file and connection is working

--- src/main/java/com/spring/springjdbc/App.java
+++ src/main/java/com/spring/springjdbc/App.java
@@ -1,5 +1,9 @@
 package com.spring.springjdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Program Started...." );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/springjdbc/springjdbc.xml");
+        JdbcTemplate template = (JdbcTemplate) context.getBean("jdbcTemplate");
+        String query ="insert into student(sId, sName, sCity) values(?, ?, ?)";
+        int rows = template.update(query, 102, "Kajal", "Noida");
+        System.out.println(rows);
     }
 }
